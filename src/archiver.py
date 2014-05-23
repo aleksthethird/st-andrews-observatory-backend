8633b54c9cf5585b38bd40b1ea3ff1c039ace6c3
billytrend <me@billytrend.com>
2014-05-23 11:22:29 +0100

    Implemented indexing and dumping.

---
+++ src/archiver.py
@@ -0,0 +1,37 @@
+import json
+import os
+import unittest
+import glob
+
+class archiver:
+
+    def __init__(self):
+        pass
+
+
+    def dump_dated_object(self, epoch, object, out_root):
+        out_file_path = os.path.join(out_root, str(epoch) + '.json')
+        while os.path.exists(out_file_path):
+            out_file_path += ".d"
+        with open(out_file_path, "w+") as file:
+            json.dump(object, file)
+
+    def index_files(self, root):
+        return {
+            'root' : root,
+            'files' : glob.glob(os.path.join(root, '*.json*'))
+        }
+
+
+class test_archiver(unittest.TestCase):
+
+    def setUp(self):
+        self.arch = archiver()
+
+    def test_dump_dated_object(self):
+        test = {}
+        self.arch.dump_dated_object(123456789, test, "../out")
+        self.assertTrue(os.path.exists("../out/123456789.json"))
+
+    def test_index(self):
+        self.assertGreater(len(self.arch.index_files('../out')['files']), 0)
--- src/parse-fits.py
+++ src/parse-fits.py
@@ -1,10 +1,11 @@
+import json
+import os
+import re
 import unittest
 from astropy.io import fits
-import scipy
 
 
 class parse_fits:
-
     def __init__(self):
         pass
 
@@ -26,8 +27,28 @@ class parse_fits:
         fits_file.close()
         return obj
 
+    def index_directory_of_fits_files(self, root, completed=set()):
+        index = {
+            'files': []
+        }
+        for path, dirnames, filenames in os.walk(root):
+            rel_path = [os.path.join(path, f) for f in filenames]
+            index['files'] += [self.get_meta(file) for file in rel_path
+                                if re.match(".fits", os.path.splitext(file)[1])
+                                and file not in completed]
+        return index
+
+    def dump_index(self, path_in, path_out, completed=set()):
+        to_dump = {}
+        if len(completed) is not 0:
+            with open(path_out, "r") as file:
+                to_dump = json.load(file)
+        with open(path_out, "w+") as file:
+            to_dump.update(self.index_directory_of_fits_files(file))
+            json.dump(to_dump)
 
 class ParseFistTest(unittest.TestCase):
+
     def setUp(self):
         self.parser = parse_fits()
         self.fits_test_file = "../test-files/test.fits"
@@ -35,5 +56,5 @@ class ParseFistTest(unittest.TestCase):
     def test_parse_meta(self):
         self.parser.get_meta(self.fits_test_file)
 
-
-
+    def test_indexing(self):
+        self.parser.index_directory_of_fits_files('../')
